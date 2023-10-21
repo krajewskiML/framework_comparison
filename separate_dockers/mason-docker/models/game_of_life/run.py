9a937122aa95e3007a1eada5171fa9e9b56422c3
paulinagacek <paulina.gacek.pl@gmail.com>
2023-10-21 14:35:36 +0000

    mason logs

--- separate_dockers/mason-docker/models/game_of_life/run.py
+++ separate_dockers/mason-docker/models/game_of_life/run.py
@@ -5,11 +5,12 @@ import sys
 
 # MODEL_FOLDER = './models/game_of_life'
 MAPS_FOLDER = './maps'
-INPUT_NAMES = ['10x10board.csv', '100x100board.csv', '1000x1000board.csv']
+INPUT_NAMES = ['10x10board.csv']
+# INPUT_NAMES = ['10x10board.csv', '100x100board.csv', '1000x1000board.csv']
 JAR_PATH = './source/golSimulation.jar'
 
-REPETITIONS = 1
-NUM_STEPS = 100  # Probably should be in a separate file since its same for all simulations
+REPETITIONS = 10000
+NUM_STEPS = 10000  # Probably should be in a separate file since its same for all simulations
 
 directory = os.getcwd()
 
@@ -19,4 +20,8 @@ for input_name in INPUT_NAMES:
         subprocess.run(
             ['java', '-jar', JAR_PATH, os.path.join(MAPS_FOLDER,  input_name), str(NUM_STEPS)])
     end = time.time()
-    print((end - start) / REPETITIONS, flush=True)
+    # print((end - start) / REPETITIONS, flush=True)
+    print("input: {}, rep: {}, iterations: {}".format(input, REPETITIONS, NUM_STEPS))
+    print("1 iteration time:",(end - start)/(REPETITIONS*NUM_STEPS))
+    print("Total time: ", (end - start))
+    print("---")
