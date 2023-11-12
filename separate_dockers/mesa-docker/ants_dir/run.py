840d8d5bc6555b34d78986fdb556557d9c6a0343
Maciej Krajewski <krajek@student.agh.edu.pl>
2023-11-12 16:24:58 +0100

    mesa ants run file

--- separate_dockers/mesa-docker/ants_dir/run.py
+++ separate_dockers/mesa-docker/ants_dir/run.py
@@ -3,9 +3,12 @@ import subprocess
 import os
 
 REPETITIONS = 2
-start = time.time()
-for rep in range(REPETITIONS):
-    subprocess.run(['python3', os.path.join('mrowki', 'run.py'), "100", "40000"])
+MAP_SIZES = ["100", "300", "500"]
+STEPS = ["40000", "10000", "3000"]
 
-end = time.time()
-print((end - start)/REPETITIONS, flush=True)
+for map_size, steps in zip(MAP_SIZES, STEPS):
+    start = time.time()
+    for rep in range(REPETITIONS):
+        subprocess.run(['python3', os.path.join('mrowki', 'run.py'), map_size, steps])
+    end = time.time()
+    print((end - start)/REPETITIONS, flush=True)
