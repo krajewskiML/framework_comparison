1951381dc312a47c98853a6fbe33b58d3dd26ac0
bartosz <bartoszkruczek@gmail.com>
2024-02-15 23:40:09 +0100

    ants sim works and fulfill all reqs

--- separate_dockers/mason-docker/models/ants_colony/run.py
+++ separate_dockers/mason-docker/models/ants_colony/run.py
@@ -3,7 +3,7 @@ import subprocess
 import os
 import sys
 
-JAR_PATH = './source/antsSimulation.jar'
+JAR_PATH = './source/golSimulation.jar'
 
 REPETITIONS = 3
 MAP_SIZES = [100, 300, 500]
@@ -12,10 +12,12 @@ directory = os.getcwd()
 
 start = time.time()
 
+steps = 10
+
 for size in MAP_SIZES:
     start = time.time()
     for rep in range(REPETITIONS):
         subprocess.run(
-            ['java', '-jar', JAR_PATH, str(size)])
+            ['java', '-jar', JAR_PATH, str(size), str(steps)])
     end = time.time()
     print((end - start) / REPETITIONS, flush=True)
